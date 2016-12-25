1db6f74ed78b3c41fbf2cd58a0ebf25e1c5e8c3b
jbwmn <jacobtylerbowman@gmail.com>
2016-12-25 02:51:05 -0600

    Add files via upload

---
+++ christmas/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+  background(0);
+  fill(255, 0, 0);
+  rect(50, 50, 50, 50);
+  rect(170, 50, 50, 50);
+  
+  fill(0, 255, 0);
+  rect(110, 50, 50, 50);
+  rect(230, 50, 50, 50);
+
+}
